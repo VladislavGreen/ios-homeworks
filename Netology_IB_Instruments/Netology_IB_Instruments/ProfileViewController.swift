fb9753b192c69d4f7fcf37a560b50b74a7ef3e19
Vladislav Green <vladzel@gmail.com>
2022-08-13 19:16:55 +0300

    Домашнее задание iOS UI 1.2 Владислав Грин IOS-24

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Vladislav Green on 8/13/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    
+}
