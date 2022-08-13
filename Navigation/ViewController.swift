2713b49fe2c2cc3e43a02032f438e12f9e9b115c
Vladislav Green <vladzel@gmail.com>
2022-08-13 16:11:36 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Vladislav Green on 8/13/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
